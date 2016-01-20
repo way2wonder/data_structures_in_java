8894d55ac9c46edb748933134f4ade8a15b0e060
way2wonder <772755185@qq.com>
2016-01-20 15:51:09 +0800

    init

---
+++ src/main/java/com/feifan/datastructures/App.java
@@ -0,0 +1,13 @@
+package com.feifan.datastructures;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
